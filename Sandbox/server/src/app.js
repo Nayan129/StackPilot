28a0b6ef14197e0cc457bfcfad9d5b0271289d03
Nayan <nayanbhusari02@gmail.com>
2026-05-16 10:35:49 +0530

    feat: implement sandbox creation with Kubernetes integration and update Dockerfile

--- Sandbox/server/src/app.js
+++ Sandbox/server/src/app.js
@@ -1,19 +1,41 @@
 import express from "express";
 import morgan from "morgan";
+import { createPod } from "./kubernetes/pod.js";
+import { createService } from "./kubernetes/service.js";
+import { v7 as uuid } from "uuid";
 
 const app = express();
 
-app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const PORT = 3000;
 
 app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({
-    message: "sandbox API is healthy",
-    status: "Ok",
+    message: "Sandbox API is healthy",
+    status: "ok",
   });
+});
+
+app.post("/api/sandbox/start", async (req, res) => {
+  try {
+    const sandboxId = uuid();
+
+    await Promise.all([createPod(sandboxId), createService(sandboxId)]);
+
+    return res.status(201).json({
+      message: "Sandbox environment created successfully",
+      sandboxId,
+      previewUrl: `http://${sandboxId}.preview.localhost`,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Failed to create sandbox",
+      error: error.message,
+    });
+  }
 });
 
 export default app;
